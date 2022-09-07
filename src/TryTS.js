c3f658a152799be3514e43360343e2ec583d17af
wholyinfa <al.acrv@outlook.com>
2022-09-07 21:04:37 +0200

    Still wondering where TS can be useful after taking a whole course on it xD

--- src/TryTS.js
+++ src/TryTS.js
@@ -1,2 +1,12 @@
 "use strict";
-let aVar = 'Eyo';
+const UserData = (data) => {
+    console.log(data);
+};
+UserData([
+    {
+        name: 'Yasmin',
+        age: 22,
+        address: 'Cat wifer Street 22, 2304 Caterson',
+        vip: true
+    }
+]);
--- src/TryTS.ts
+++ src/TryTS.ts
@@ -1,1 +1,34 @@
-let aVar:string = 'Eyo';
+interface data {
+    name: string;
+    age: number;
+    address: string;
+    vip: boolean;
+    phone?: phone;
+}
+interface phone {
+    model: string;
+    brand: string;
+    space: space;
+    resolution: resolution;
+}
+interface space {
+    size: number;
+    unit: string;
+}
+interface resolution {
+    size: number;
+    unit: string;
+}
+
+const UserData = (data: data[]): void => {
+    console.log(data);
+}
+
+UserData([
+    {
+        name: 'Yasmin',
+        age: 22,
+        address: 'Cat wifer Street 22, 2304 Caterson',
+        vip: true
+    }
+]);

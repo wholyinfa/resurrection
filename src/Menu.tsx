a86f19c9273bbc143197436b9a0408015ee84d27
wholyinfa <al.acrv@outlook.com>
2022-09-12 17:05:08 +0200

    Add the initia script file of the Menu

---
+++ src/Menu.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+
+export const Menu = () => {
+
+    return <nav>
+        <NavLink to={'/'}>
+
+        </NavLink>
+    </nav>;
+}

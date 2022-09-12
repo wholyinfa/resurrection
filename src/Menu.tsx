13ee4659557a8dafb7d3f990c3db214eab5c0644
wholyinfa <al.acrv@outlook.com>
2022-09-12 22:05:47 +0200

    Initialize centering the active menu item

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { titleConversion } from "./App";
 import { PageData, Pages } from "./data";
 import './Stylesheets/menu.css';
 
 export const Menu = () => {
 
+    const location = useLocation();
     const itemList: PageData[] = [];
+    const activePage: PageData = Object.values(Pages).filter( entry => entry.url === titleConversion(location.pathname) )[0];
     Object.keys(Pages).map( item => {
         itemList.push(Pages[item as keyof Pages]);
     });
+
+    useEffect( () => {
+        
+    }, [location]);
+
     const [items, setItems] = useState<PageData[]>(itemList);
     const [checkItems, setCheckItems] = useState<boolean>();
 
@@ -17,11 +25,13 @@ export const Menu = () => {
 
     }, [checkItems]);
 
-    return <nav>
+    return <nav id='dialer'>
+        <div>
         { items.map( (item, i) => {
-            return <NavLink key={i} to={item.url}>
+            return <NavLink exact key={i} to={item.url}>
                 {item.text}
             </NavLink>;
         }) }
+        </div>
     </nav>;
 }

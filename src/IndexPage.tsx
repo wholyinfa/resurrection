23e72c7f27a6b140285c51affc2b2ec9cc1a8161
wholyinfa <al.acrv@outlook.com>
2022-09-16 16:43:06 +0200

    Set up the base structure of the index page

--- src/IndexPage.tsx
+++ src/IndexPage.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 import './Stylesheets/index.css';
 export default function IndexPage() {
-    return <h1>I'm index</h1>;
+
+
+
+    return <article id='homePage'>
+        <div id='overTakers'>
+            <div className='work'></div>
+            <div className='life'></div>
+        </div>
+        <div id='division'>
+            <div className='work'>
+                <div className='treeBrain'></div>
+                <button className='borderButton'>WORK</button>
+            </div>
+            <div className='life'>
+                <div className='treeBrain'></div>
+                <button className='borderButton'>LIFE</button>
+            </div>
+        </div>
+        <div className='theBreathingFragment'></div>
+    </article>;
 }

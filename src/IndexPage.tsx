9ccaeefc38002a37422055c12635cbe8eb792514
wholyinfa <al.acrv@outlook.com>
2022-09-16 22:12:27 +0200

    Add the takeover animation on work/life button clicks

--- src/IndexPage.tsx
+++ src/IndexPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import './Stylesheets/index.css';
 
@@ -7,26 +7,63 @@ export default function IndexPage() {
 
     let workHover: gsap.core.Timeline;
     let lifeHover: gsap.core.Timeline;
+    let workResurrection: gsap.core.Timeline;
+    let lifeResurrection: gsap.core.Timeline;
     useEffect(() => {
-        if( typeof workHover === 'undefined' ){
-            workHover = gsap.timeline({paused: true});
-            workHover.fromTo('#overTakers .work',
-            {background: 'linear-gradient(45deg, rgba(26,35,126,1) 0%, rgba(0,0,0,0) 23%)'},
-            {background: 'linear-gradient(45deg, rgba(26,35,126,1) 0%, rgba(0,0,0,0) 33%)',
+        
+        const setHover = (type: 'work' | 'life') => {
+            let timeline = gsap.timeline({paused: true});
+            let color = (type === 'work') ? 'rgba(26,35,126,1)' : 'rgba(6,78,59,1)';
+            let deg = (type === 'work') ? '45deg' : '-45deg';
+
+            timeline.fromTo(`#overTakers .${type}`,
+            {background: `linear-gradient(${deg}, ${color} 0%, rgba(0,0,0,0) 23%)`},
+            {background: `linear-gradient(${deg}, ${color} 0%, rgba(0,0,0,0) 33%)`,
              ease: 'circ.inOut',
-            duration: .2});
+             duration: .5});
+            
+            if( type === 'work' )
+                workHover = timeline;
+            else
+                lifeHover = timeline;
+        }
+        if( typeof workHover === 'undefined' ){
+            setHover('work');
         }
         if( typeof lifeHover === 'undefined' ){
-            lifeHover = gsap.timeline({paused: true});
-            lifeHover.fromTo('#overTakers .life',
-            {background: 'linear-gradient(-45deg, rgba(6,78,59,1) 0%, rgba(0,0,0,0) 23%)'},
-            {background: 'linear-gradient(-45deg, rgba(6,78,59,1) 0%, rgba(0,0,0,0) 33%)',
-             ease: 'circ.inOut',
-            duration: .2});
+            setHover('life');
         }
-    }, [])
+
+        const setResurrection = (type: 'work' | 'life') => {
+            let timeline = gsap.timeline({paused: true});
+            let color = (type === 'work') ? 'rgba(26,35,126,1)' : 'rgba(6,78,59,1)';
+            let deg = (type === 'work') ? '45deg' : '-45deg';
+            let opposite = (type === 'work') ? '.life' : '.work';
+
+            timeline
+            .to('#division button, #division .treeBrain, #homePage .breathingFragment', {autoAlpha: 0})
+            .to(`#overTakers ${opposite}`, {autoAlpha: 0}, '<')
+            .fromTo(`#overTakers .${type}`,
+                {background: `linear-gradient(${deg}, ${color} 0%, rgba(0,0,0,0) 23%)`},
+                {background: `linear-gradient(${deg}, ${color} 0%, rgba(0,0,0,0) 200%)`}, '<')
+            .to('body', {background: color}, '.1<')
+            .to(`#overTakers .${type}`, {autoAlpha: 0});
+            
+            if( type === 'work' )
+                workResurrection = timeline;
+            else
+                lifeResurrection = timeline;
+        }
+        if( typeof lifeResurrection === 'undefined' ){
+            setResurrection('life');
+        }
+        if( typeof workResurrection === 'undefined' ){
+            setResurrection('work');
+        }
+    }, []);
 
     const handleMouseEnter = (type?: 'life' | 'work') => {
+        if (resurrecting.current) return; 
         if( type === 'work' ){
             workHover.play();
         }else{
@@ -34,10 +71,24 @@ export default function IndexPage() {
         }
     }
     const handleMouseLeave = (type?: 'life' | 'work') => {
+        if (resurrecting.current) return; 
         if( type === 'work' ){
             workHover.reverse();
         }else{
             lifeHover.reverse();
+        }
+    }
+
+    const applyResurrection = () => {
+
+    }
+    const resurrecting = useRef<boolean>(false);
+    const handleClick = (type?: 'life' | 'work') => {
+        resurrecting.current = true;
+        if( type === 'work' ){
+            workResurrection.play();
+        }else{
+            lifeResurrection.play();
         }
     }
 
@@ -50,12 +101,14 @@ export default function IndexPage() {
             <div className='work'>
                 <div className='treeBrain'></div>
                 <button className='borderButton'
+                onClick={() => handleClick('work')}
                 onMouseEnter={() => handleMouseEnter('work')}
                 onMouseLeave={() => handleMouseLeave('work')}><span>WORK</span></button>
             </div>
             <div className='life'>
                 <div className='treeBrain'></div>
                 <button className='borderButton'
+                onClick={() => handleClick('life')}
                 onMouseEnter={() => handleMouseEnter('life')}
                 onMouseLeave={() => handleMouseLeave('life')}><span>LIFE</span></button>
             </div>

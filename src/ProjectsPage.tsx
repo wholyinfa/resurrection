cad2ed33fc901b5a3fbe0749cab64f47c51e2a2c
wholyinfa <al.acrv@outlook.com>
2022-10-02 21:39:57 +0200

    Include the structure of Projects page

--- src/ProjectsPage.tsx
+++ src/ProjectsPage.tsx
@@ -2,11 +2,32 @@ import React, { useEffect } from 'react';
 import './Stylesheets/projects.css';
 import PropTypes, {InferProps} from 'prop-types';
 
+const Card = () => {
+    return <div className='Card'>
+        <img src='' alt='' />
+        <div className='content'>
+            <p className='preTitle'>Project</p>
+            <h2 className='title'>Project Title</h2>
+            <p className='brief'>
+                Brief description of this project...
+            </p>
+            <button className='charcoalButton'>VIEW</button>
+        </div>
+    </div>;
+}
 export default function ProjectsPage({}: InferProps<typeof ProjectsPage.propTypes>) {
     useEffect(() => {
         
     }, [])
-    return <h1>I'm projects</h1>;
+    return <article id='projectsPage'>
+    <div className='title'>
+        <h1>PROJECTS</h1>
+        <div>PROJECTS</div>
+    </div>
+    <section>
+        <Card />
+    </section>
+</article>;
 }
 ProjectsPage.propTypes ={
 

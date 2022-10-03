e796842ad265d73f524df42286565a3eb1692ad2
wholyinfa <al.acrv@outlook.com>
2022-10-03 08:20:30 +0200

    Include basic styling of project preview cards

--- src/ProjectsPage.tsx
+++ src/ProjectsPage.tsx
@@ -5,12 +5,12 @@ import { Projects } from './data';
 import { Link } from 'react-router-dom';
 
 function Card({title, context, url, imgSource}: InferProps<typeof Card.propTypes>) {
-    return <div className='Card'>
-        <img src={imgSource} alt={`Preview sample of ${title} project`} />
+    return <div className='card cobalt'>
+        <div className='img' style={{backgroundImage: `url('${imgSource}')`}} />
         <div className='content'>
             <p className='preTitle'>Project</p>
             <h2 className='title'>{title}</h2>
-            <p className='brief'>{context}</p>
+            <p className='brief'>{context+'...'}</p>
             <Link to={url} className='charcoalButton'>VIEW</Link>
         </div>
     </div>;
@@ -32,10 +32,11 @@ export default function ProjectsPage({}: InferProps<typeof ProjectsPage.propType
     </div>
     <section>
         {
-            Projects.map(project => {
+            Projects.map( (project, i) => {
                 return <Card
+                    key = {i}
                     title = {project.title}
-                    context = {project.context}
+                    context = {project.context.slice(0, 100)}
                     url = {project.url}
                     imgSource = {project.imgSource.preview}
                 />

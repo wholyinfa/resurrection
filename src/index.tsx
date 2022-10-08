fa0348f87f4c50c642f3e849b261848f6963d9bb
wholyinfa <al.acrv@outlook.com>
2022-10-08 07:56:23 +0200

    Disable strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <Router>
-    <React.StrictMode>
       <main>
         <App />
       </main>
-    </React.StrictMode>
   </Router>
 );
